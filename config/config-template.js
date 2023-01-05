9979300f52b5b3087c06160db02d0f7977b71ed7
ccoincash <ccoincash@gmail.com>
2023-01-05 23:15:18 +0800

    add README

--- config/config-template.js
+++ config/config-template.js
@@ -13,10 +13,11 @@ const config = {
         ip: '127.0.0.1',
         port: 35001,
     },
+    // this is a config for mongodb
     db: {
         name: 'mvc-mns-testnet', // db name
         collection: 'name', // collection name
-        uri: 'mongodb://user:password@ip:port',
+        uri: 'mongodb://user:password@ip:port', // db uri
         max_concurrency: 30
     },
     mnsCodeHash: '',
