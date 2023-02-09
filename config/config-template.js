0fb1f8f992dff330e8bf0c6bac0bb10c84bd59cd
ccoincash <ccoincash@gmail.com>
2023-02-09 11:22:04 +0800

    change config

--- config/config-template.js
+++ config/config-template.js
@@ -1,7 +1,8 @@
 "use strict";
 const config = {
     dataService: {
-        tokenUrl: 'https://api-mvc-testnet.metasv.com',
+        tokenUrl: 'https://api-mvc.metasv.com', // mainnet
+        //tokenUrl: 'https://api-mvc-testnet.metasv.com', // testnet
     },
     log: {
         path: './logs/',
@@ -15,12 +16,13 @@ const config = {
     },
     // this is a config for mongodb
     db: {
-        name: 'mvc-mns-testnet', // db name
+        name: 'mvc-mns', // db name
         collection: 'name', // collection name
         uri: 'mongodb://user:password@ip:port', // db uri
     },
     mnsCodeHash: '38b0a77b5c84bfaba8b8cc39f729175c67111672',
-    mnsId: '4b45149524803eafd4670ca79af72cabaf9bc5df',
+    mnsId: ' c6f4ef79b4041e7928f6fe47b6874ba92646d894', // mainnet
+    // mnsId: 'f33cf834cd29ebfa725529340d33997427ea2792', // testnet
     syncInterval: 30000, // ms
 };
 exports.config = config;
